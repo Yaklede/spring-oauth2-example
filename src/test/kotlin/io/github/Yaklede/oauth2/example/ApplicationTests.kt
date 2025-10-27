ccd57ddcde67e84d3a8f974604ab9199c1d7394c
Yaklede <chh78151@naver.com>
2025-10-28 08:41:40 +0900

    feature: spring project init

---
+++ src/test/kotlin/io/github/Yaklede/oauth2/example/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.Yaklede.oauth2.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
