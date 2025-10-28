5de2b89ffd608dd74b45ff0b91290bf3b84bdf6a
Yaklede <chh78151@naver.com>
2025-10-28 10:04:07 +0900

    feature: db설정 추가

--- modules/infrastructure/oauth-client-infra/build.gradle.kts
+++ modules/infrastructure/oauth-client-infra/build.gradle.kts
@@ -21,4 +21,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.mariadb.jdbc:mariadb-java-client")
 }
--- modules/infrastructure/oauth-resource-infra/build.gradle.kts
+++ modules/infrastructure/oauth-resource-infra/build.gradle.kts
@@ -21,4 +21,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.mariadb.jdbc:mariadb-java-client")
 }
